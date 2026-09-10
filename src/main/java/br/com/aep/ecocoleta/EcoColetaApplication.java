163cc17e2bc386e27205313478356bfde5563bf6
Alberto-Neo <neoalberto15@gmail.com>
2026-09-10 17:26:37 -0300

    estrutura inicial

---
+++ src/main/java/br/com/aep/ecocoleta/EcoColetaApplication.java
@@ -0,0 +1,11 @@
+package br.com.aep.ecocoleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoColetaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EcoColetaApplication.class, args);
+    }
+}
